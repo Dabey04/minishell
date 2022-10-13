a5f339a999893b83906cb4c54450a661e642bc74
Dabey04 <66482999+Dabey04@users.noreply.github.com>
2022-10-13 19:59:12 +0200

    add file

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,1 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dabey <dabey@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/13 15:47:59 by dabey             #+#    #+#             */
+/*   Updated: 2022/10/13 18:23:52 by dabey            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+//***********************ft_uils********************
+
+char	**ft_split(char *s, char c);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
