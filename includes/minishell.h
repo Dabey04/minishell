bbbf1712647661035d2c970848d84dcc44983d13
Dabey04 <66482999+Dabey04@users.noreply.github.com>
2022-10-18 19:54:01 +0200

    Add files via upload

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: dabey <dabey@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 15:47:59 by dabey             #+#    #+#             */
-/*   Updated: 2022/10/13 18:23:52 by dabey            ###   ########.fr       */
+/*   Updated: 2022/10/18 19:41:04 by dabey            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,29 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <errno.h>
+
+typedef	enum	e_type
+{
+	WORD = 1,
+	QUATE = 2,
+	COMMANDE = 3,
+	ARG = 4,
+	OPTION = 5,
+}enum_type;
+
+typedef struct s_list
+{
+	enum_type		type;
+	char		*buffer;
+	char		**cmd;
+	size_t		buf_size;
+	int			nb_quate; // = -1 si ya pas et nb si y on a
+	int			nb_db_quate; // = -1 si ya pas 
+}t_list;
 
 //***********************ft_uils********************
-
+t_list	*ft_init(t_list *list);
 char	**ft_split(char *s, char c);
 int		ft_strcmp(char *s1, char *s2);
 
